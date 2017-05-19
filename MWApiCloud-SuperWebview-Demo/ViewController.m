57cf4cfd29e9750b6dee85538ac0e21ab86db06b
MakeItEasy <rugang6891@gmail.com>
2017-05-19 14:33:50 +0800

    添加了webview方式和widget方式的注释说明

--- MWApiCloud-SuperWebview-Demo/ViewController.m
+++ MWApiCloud-SuperWebview-Demo/ViewController.m
@@ -32,16 +32,25 @@
 //    [self openWebViewWithWindowType];
 }
 
+/**
+ 以WebView的方式打开页面，就相当于打开了一个ViewController
+ 这种方式下，自己可以在本地创建NavViewController，然后push WebView页面
+ 主要是方面集成到现有的本地页面中
+ */
 - (void)openWebViewWithWindowType {
     // 这里的widget://表示widget的根目录路径
     NSString *url = @"widget://index.html";
-    NSString *name = @"web view controller";
+    NSString *name = @"index";
     MWWebViewController *webVC = [MWWebViewController windowContainerWithAttribute:@{@"url":url, @"name":name}];
     [webVC startLoad];
     [self.navigationController pushViewController:webVC animated:YES];
 //    [self presentViewController:webVC animated:YES completion:nil];
 }
 
+
+/**
+ 以widget的方式打开页面，相当于打开了一个NavigationViewController
+ */
 - (void)openWebViewWithWidgetType {
     // 这里的widget://表示widget的根目录路径
      NSString *url = @"widget://index.html";
